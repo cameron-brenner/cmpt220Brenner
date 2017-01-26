0c53621bda19fe4895b295e6a56bb83f7efa963e
CameronBrenner <CameronBrenner>
2017-01-26 17:28:18 -0500

    Cam Brenner 1/26/16

--- labs/Lab 1/Lab1_21.java
+++ labs/Lab 1/Lab1_21.java
@@ -2,10 +2,10 @@ import java.util.Scanner;
 
 public class Lab1_21 {
   public static void main(String[] args) {
-	Scanner input = new Scanner (System.in);
+  Scanner input = new Scanner (System.in);
     System.out.print("Enter the degrees in Celsius: ");
-	double celsius = input.nextDouble();
-	double farenheit = ((9.0 / 5) * celsius + 32) ;
-	System.out.println(celsius + " degrees celsius are " + farenheit + " degrees farenheit");
+    double celsius = input.nextDouble();
+    double farenheit = ((9.0 / 5) * celsius + 32) ;
+    System.out.println(celsius + " degrees celsius are " + farenheit + " degrees farenheit");
   }
 }
--- labs/Lab 1/Lab1_grades.java
+++ labs/Lab 1/Lab1_grades.java
@@ -7,21 +7,21 @@ import java.util.Scanner;
 
 public class Lab1_grades {
   public static void main(String[] args) {
-	Scanner input = new Scanner (System.in);
+    Scanner input = new Scanner (System.in);
     System.out.println("Please enter the following as percentage...");
-	System.out.print("Midterm Exam: ");
-	  double midterm = input.nextDouble();
-	  double midtermpercent = (midterm * .2);
-	System.out.print("Final Exam: ");
-	  double finalex = input.nextDouble();
-	  double finalexpercent = (finalex * .2);
-	System.out.print("Projects: ");
-	  double project = input.nextDouble();
-	  double projectpercent = (project * .2);
-	System.out.print("Homework and Labs: ");
-	  double hwlabs = input.nextDouble();
-	  double hwlabspercent = (hwlabs * .4);
-	double finalgrade = (midtermpercent + finalexpercent + projectpercent + hwlabspercent);
+    System.out.print("Midterm Exam: ");
+      double midterm = input.nextDouble();
+      double midtermpercent = (midterm * .2);
+    System.out.print("Final Exam: ");
+      double finalex = input.nextDouble();
+      double finalexpercent = (finalex * .2);
+    System.out.print("Projects: ");
+      double project = input.nextDouble();
+      double projectpercent = (project * .2);
+    System.out.print("Homework and Labs: ");
+      double hwlabs = input.nextDouble();
+      double hwlabspercent = (hwlabs * .4);
+    double finalgrade = (midtermpercent + finalexpercent + projectpercent + hwlabspercent);
 	System.out.println("Your final grade is: " + finalgrade);
 	
   }
